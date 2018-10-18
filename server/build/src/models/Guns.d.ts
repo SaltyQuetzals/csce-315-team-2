5f9043f39f22891367a847fa68db436c88affe93
Gabriel Britain <quontas@gmail.com>
2018-10-18 15:28:57 -0500

    Abstracted bullet-loading process into its own function.

--- server/build/src/models/Guns.d.ts
+++ server/build/src/models/Guns.d.ts
@@ -1,13 +1,15 @@
 export declare abstract class Gun {
-    readonly fireRateInMillis: number;
+    readonly fireRateMillis: number;
+    readonly reloadRateMillis: number;
     protected readonly damage: number;
     protected readonly clipSize: number;
     protected shotsInClip: number;
     protected canFire: boolean;
     ammoRemaining: number;
-    constructor(fireRateInMillis: number, damage: number, clipSize: number, ammoRemaining: number);
+    constructor(fireRateMillis: number, reloadRateMillis: number, damage: number, clipSize: number, ammoRemaining: number);
     fire(firingFunction?: Function): void;
     private readonly canFillClip;
+    private loadBullets();
     reload(): void;
     readonly fireable: boolean;
     readonly shotsRemaining: number;

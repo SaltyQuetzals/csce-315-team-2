ff1490153d5086345143569d51091316b7f51577
Gabriel Britain <quontas@gmail.com>
2018-10-26 13:46:38 -0500

    In-progress sockets on backend

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,14 @@ import path = require('path');
 import * as socketio from 'socket.io';
 import http = require('http');
 
+const ROOM_CODE_LENGTH = 5;
+
+function random(len: number) {
+  return Math.random().toString(36).substr(2, len);
+}
 
 
 const app = express();
-const server = new http.Server(app);
-const io = socketio(server);
-
-
-server.listen(3000, () => {
-  console.log('Listening on port 3000');
-});
-
 
 const STATIC_DIR = path.join(__dirname, 'public');
 app.use(express.static(STATIC_DIR));
@@ -22,14 +19,33 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(STATIC_DIR, 'index.html'));
 });
 
+
+const server = new http.Server(app);
+const io = socketio(server);
+
+server.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
+
 io.on('connection', (socket) => {
-  console.log('Client connected!');
-  socket.on('disconnect', () => {
-    console.log('Client disconnected.');
+
+  // Create a new room and notify creator
+  socket.on('create', (data) => {
+    const roomCode = random(ROOM_CODE_LENGTH);
+    socket.join(`room-${roomCode}`);
   });
 
-  socket.on('room', (room) => {
-    console.log(`A request to join room ${room} has occurred.`);
-    socket.join(room);
+  socket.on('joinGame', (data) => {
+    const room = io.nsps['/'].adapter.rooms[data.room];
+    if (!room) {
+      socket.emit('err', {message: 'The room you requested does not exist.'});
+    } else {
+      if (room.length > 10) {
+        socket.emit('err', {message: 'The room you requested is full. Please try again later.'});
+      } else {
+        socket.join(data.room);
+        socket.broadcast.to(data.room).emit('new player', {});
+      }
+    }
   });
 });

b8beb921bcaf7613c1e1acdd1b177b6ebbad67c9
Gabriel Britain <quontas@gmail.com>
2018-10-30 14:16:14 -0500

    Removed activate from PowerUp models, will be handled by front-end

--- src/models/PowerUp.ts
+++ src/models/PowerUp.ts
@@ -1,14 +1,5 @@
-import {delay} from '../shared/functions';
-
 export abstract class PowerUp {
-  active = false;
   constructor(public duration: number, public type: PowerUpType) {}
-
-  async activate(): Promise<void> {
-    this.active = true;
-    await delay(this.duration);
-    this.active = false;
-  }
 }
 
 export enum PowerUpType {

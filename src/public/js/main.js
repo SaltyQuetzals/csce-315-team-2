84ed977155c8cd2be3be4d600366055f4fff56eb
Nick Nelson <nnelson1601@tamu.edu>
2018-11-03 21:19:48 -0500

    Quick Fixes

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -271,8 +271,6 @@ function bulletHitHandler(bullet, enemy) {
 
 function movementHandler(avatar, gun, keys, /*pos = {x: false,y: false}*/ ) {
     let eventShouldBeEmitted = false;
-    const origZombieX = Number(avatar.body.x);
-    const origZombieY = Number(avatar.body.y);
 
     if (keys['left']) {
         avatar.body.velocity.x = -ZOMBIE_SPEED;
@@ -346,12 +344,11 @@ function movementHandler(avatar, gun, keys, /*pos = {x: false,y: false}*/ ) {
         //zombie.anims.play('idle');
     }
     if (eventShouldBeEmitted) {
-        console.log(avatar.body.x - origZombieX);
         socket.emit("move", {
             roomId,
             location: {
-                x: avatar.body.x,
-                y: avatar.body.y
+                x: Number(avatar.body.x),
+                y: Number(avatar.body.y)
             }
         });
     }
